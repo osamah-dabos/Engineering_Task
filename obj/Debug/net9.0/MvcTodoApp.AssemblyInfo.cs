2c75e6047fc4e6ea34907e75b85717f46b230819
osamah <adbws029@gmail.com>
2025-06-06 23:10:23 +0300

    Add latest changes for Delete Task feature

--- obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10937cb78a0747c3824ae920fa96192c8d4eed30")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
